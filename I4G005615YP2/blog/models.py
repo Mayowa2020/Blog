65a1923317821041c5fa589f576487235d26c7f7
Mayowa2020 <73591664+Mayowa2020@users.noreply.github.com>
2022-06-22 21:43:09 +0100

    added white  spaces in my code to make it look neat

--- I4G005615YP2/blog/models.py
+++ I4G005615YP2/blog/models.py
@@ -2,6 +2,7 @@ from venv import create
 from django.db import models
 from django.contrib.auth import get_user_model
 User = get_user_model()
+
 # Create your models here.
 class Post(models.Model):
     title = models.CharField(max_length=200, unique=True)
@@ -9,6 +10,8 @@ class Post(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     created_date = models.DateTimeField(auto_now_add=True)
     published_date = models.DateTimeField(auto_now=True)
+
+
     def __str__(self):
         return f'{str(self.author)} Blog.Title: {self.title}'
     
