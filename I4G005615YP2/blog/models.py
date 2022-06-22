c654da25be619c13a96609bfc988dce160cd5ffd
Mayowa2020 <73591664+Mayowa2020@users.noreply.github.com>
2022-06-22 17:15:28 +0100

    created the Post Model in the blog app, created and ran all migrations

--- I4G005615YP2/blog/models.py
+++ I4G005615YP2/blog/models.py
@@ -1,3 +1,15 @@
+from venv import create
 from django.db import models
-
+from django.contrib.auth import get_user_model
+User = get_user_model()
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=200, unique=True)
+    text = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_date = models.DateTimeField(auto_now_add=True)
+    published_date = models.DateTimeField(auto_now=True)
+    def __str__(self):
+        return f'{str(self.author)} Blog.Title: {self.title}'
+    
+ 
